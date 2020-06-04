e512f5fcc79c0470d1bc492a2ed94032f83eb88b
nekkunj <nekkunjpilani@gmail.com>
2020-06-04 20:21:08 +0530

    initial commit

--- api/entry.js
+++ api/entry.js
@@ -6,23 +6,21 @@ module.exports=function(app){
 
 
 app.post('/application_form',(req,res)=>{
+console.log(req.body);
+application_form_one=new application_form();
+application_form_one.firstname=req.body.first;
+application_form_one.lastname=req.body.last
+application_form_one.parentfirstname=req.body.parentfirst
+application_form_one.parentlastname=req.body.parentlast
+application_form_one.city=req.body.city
+application_form_one.email=req.body.email
+application_form_one.phone=req.body.phone
+application_form_one.save()
+.then(one=>{
+console.log('Entered Successfully');
 res.redirect('/successful_details')
-
-// console.log(req.body);
-// application_form_one=new application_form();
-// application_form_one.firstname=req.body.first;
-// application_form_one.lastname=req.body.last
-// application_form_one.parentfirstname=req.body.parentfirst
-// application_form_one.parentlastname=req.body.parentlast
-// application_form_one.city=req.body.city
-// application_form_one.email=req.body.email
-// application_form_one.phone=req.body.phone
-// application_form_one.save()
-// .then(one=>{
-// console.log('Entered Successfully');
-// res.redirect('/successful_details')
-// })
-//  .catch(err=>{console.log(err)})
+})
+ .catch(err=>{console.log(err)})
 
 })
 
